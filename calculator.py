2bc60e9026f43f3c32472d8fa927642741dea161
Ian White <white.it@ufl.edu>
2026-04-13 13:20:26 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -31,4 +31,24 @@ def logarthm(a, b):
 def exponent(a, b):
     return a ** b
 
+import math
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+def log(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError
+    if b <= 0:
+        raise ValueError
+    return math.log(a, b)
+def exp(a, b):
+    return a ** b
+
 
